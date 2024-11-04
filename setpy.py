e4a6ab66893a56886c9972fbdad8be572ef9a486
weihouang <willy200335@gmail.com>
2024-11-04 12:24:27 -0600

    hi

---
+++ setpy.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="test_module",
+    version="0.1",
+    description="a testing module",
+    packages=find_packages(),
+    install_requires=[],  # List your dependencies here
+    author="Wei-Ho Uang",
+    author_email="willy200335@gmail.com",
+    url="https://github.com/yourusername/my_module",  # Optional URL for your project
+)
